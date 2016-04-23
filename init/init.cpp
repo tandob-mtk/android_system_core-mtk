b3b8554c12acb85b98e5257478868421ffdfd356
fire855 <thefire855@gmail.com>
2016-04-23 14:06:29 +0200

    Set hardware to mt6592
    Change-Id: Ibe5995afe330baf0999ae819f153710b7eb42815

--- init/init.cpp
+++ init/init.cpp
@@ -817,7 +817,7 @@ static void export_kernel_boot_props() {
         { "ro.boot.mode",       "ro.bootmode",   "unknown", },
         { "ro.boot.baseband",   "ro.baseband",   "unknown", },
         { "ro.boot.bootloader", "ro.bootloader", "unknown", },
-        { "ro.boot.hardware",   "ro.hardware",   "unknown", },
+        { "ro.boot.hardware",   "ro.hardware",   "mt6592", },
 #ifndef IGNORE_RO_BOOT_REVISION
         { "ro.boot.revision",   "ro.revision",   "0", },
 #endif
